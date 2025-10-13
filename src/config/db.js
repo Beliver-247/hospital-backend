61322942b47b9e5de93bcb5aedbb0d872fa7f78f
Dumindu Mendis <121997251+Beliver-247@users.noreply.github.com>
2025-10-13 23:39:39 +0530

    config files added for URL centralization

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,11 @@ import env from './env.js';
 
 export async function connectDB() {
   mongoose.set('strictQuery', true);
-  const dbName = 'hospital-db'; // <- your required database name
-  await mongoose.connect(env.atlasUri, { dbName });
-  console.log(`[db] Connected to MongoDB Atlas -> ${dbName}`);
+
+  // IMPORTANT: we use your selected database name here
+  await mongoose.connect(env.atlasUri, {
+    dbName: 'hospital-db',
+  });
+
+  console.log('✅ MongoDB connected (hospital-db)');
 }
