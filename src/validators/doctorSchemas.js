bf15c9871417d18ae06300fc50e66fb0837c8dce
Dumindu Mendis <121997251+Beliver-247@users.noreply.github.com>
2025-10-17 19:30:30 +0530

    doctor endpoints added (create and delete by staff)

---
+++ src/validators/doctorSchemas.js
@@ -0,0 +1,7 @@
+import Joi from 'joi';
+
+export const createDoctorSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  name: Joi.string().trim().min(1).required()
+});
