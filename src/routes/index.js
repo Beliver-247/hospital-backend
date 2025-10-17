915472ad093784b5bdfdc6ed0822d73b16adf28f
pasan1112 <irindukodithuwakku12@gmail.com>
2025-10-17 18:56:22 +0530

    3

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import authRoutes from './auth.routes.js';
 import patientRoutes from './patient.routes.js';
 import uploadRoutes from './upload.routes.js';
 import reportRoutes from './report.routes.js';
+import appointmentRoutes from './appointment.routes.js'
 
 const router = Router();
 
